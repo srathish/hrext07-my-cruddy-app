c0db98125251ea208fc22213bf157db94ed1d909
Saiyeesh Rathish <srathish@Saiyeeshs-MacBook-Pro-2.local>
2019-02-12 14:03:46 -0800

    updateed

--- app/app.js
+++ app/app.js
@@ -3,7 +3,10 @@
 // using the current crudd app they can input any numbers that they want
 // this could also be a getter setter function
 // would randomly assign the numbers
-//
+
+// need to use the assign mechanism to assign to particular divs
+
+// othwrwise if no argument is set then run a random number generator.
 
 $(document).ready(function(){
   // this is where we jquery
@@ -43,6 +46,116 @@ $(document).ready(function(){
   $('.btn-clear').click(function(){
     localStorage.clear();
     $('.container-data').text('');
+  });
+
+  // creating a button for each of the squares ( whiteballs from 1 to 69 powerball goes to 29)
+ var results = $("#results");
+  results.click(function() {
+    results.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
+  });
+
+
+  var results2 = $("#results2");
+  results2.click(function() {
+    results2.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results2.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
+  });
+  
+    var results3 = $("#results3");
+  results3.click(function() {
+    results3.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results3.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
+  });
+  
+     var results4 = $("#results4");
+  results4.click(function() {
+    results4.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results4.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
+  });
+  
+      var results5 = $("#results5");
+  results5.click(function() {
+    results5.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results5.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
+  });
+
+        var results6 = $("#results6");
+  results6.click(function() {
+    results6.empty();
+     var properResults = [];
+    var rand = 0;
+    var contains = false;
+    for (i = 1; i < 2; i++) {
+        do 
+        {
+         (rand = Math.floor((Math.random() * 69) + 1));
+          contains = properResults.indexOf(rand) > -1;
+        } while(contains)
+        results6.append("<br />", rand, "<br />");
+        properResults.push(rand);
+    }
+    
   });
 
 });
